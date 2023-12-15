bf4c7afc7d410d96135256d1b8612ea8747938a6
sum008 <sk4641230@gmail.com>
2023-12-15 18:01:30 +0530

    cleaned up code

--- clean_and_save_fkart_data/utility.py
+++ clean_and_save_fkart_data/utility.py
@@ -1,7 +1,6 @@
 import requests
 
 def get_base_token(bearer):
-    # f37c8239d992ab304d31c9dcdd7158eadc98dd83
     url = "https://cloud.seatable.io/api/v2.1/dtable/app-access-token/"
     headers = {
         "accept": "application/json",
@@ -20,10 +19,6 @@ def create_table(payload, base_uuid, bearer):
 
 def insert_data_to_table(base_uuid, payload, bearer):
     url = f"https://cloud.seatable.io/dtable-server/api/v1/dtables/{base_uuid}/batch-append-rows/"
-    # payload = {
-    #     "table_name": "test_tbl",
-    #     "rows": [{ "col1": "sdfsdfsd" }, { "col1": "aaaaaaa" }, { "col1": "ttttttt" }]
-    # }
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
