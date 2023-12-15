d3489c304c50a62eaa6c17b80755eeaaab6aa220
sumit <44076169+sum008@users.noreply.github.com>
2023-12-15 09:38:15 +0530

    Update cleaner.py

--- fkart_data_cleaner/cleaner.py
+++ fkart_data_cleaner/cleaner.py
@@ -25,12 +25,8 @@ def convert_json_to_dict():
 def final_correction(header_set, spec_dict):
     for index in range(0, len(spec_dict)):
         for header in header_set:
-            # print(spec_dict[index])
             if header not in spec_dict[index].keys():
                 spec_dict[index][header] = None
-            # else:
-            #     spec_dict[index][header] = None
-        # spec_dict[index] = dict(sorted(spec_dict[index].items()))
     return spec_dict
 
 
@@ -39,17 +35,8 @@ if __name__ == "__main__":
         "C:/Users/sk464/Documents/flipkart_scraper_with_scrapy/fkart_data_scraper/specs.jsonl", lines=True)
     dict_to_list = convert_json_to_dict()
     final_headers = extract_headers()
-    # print(type(dict_to_list[0]))
-    # dict_to_list = final_correction(
-    #     header_set=final_headers, spec_dict=dict_to_list)
-    print(final_headers)
-    # print(len(dict_to_list))
-    # print(dict_to_list[1])
-    # dict_to_list = dict(sorted(dict_to_list.items()))
-    # final_df = pd.DataFrame.from_dict(dict_to_list, orient="columns")
-    # final_df.to_csv(".//converted_csv.csv", header=True, index=False)
-
-    # ddd = pd.read_csv(".//converted_csv.csv")
-    # print(ddd.head())
-
-# print(fkart_data.reset_index()["specs"])
+    dict_to_list = final_correction(
+        header_set=final_headers, spec_dict=dict_to_list)
+    dict_to_list = dict(sorted(dict_to_list.items()))
+    final_df = pd.DataFrame.from_dict(dict_to_list, orient="columns")
+    final_df.to_csv(".//converted_csv.csv", header=True, index=False)
